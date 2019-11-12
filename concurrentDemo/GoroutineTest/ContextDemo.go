300ffa672aa2e8e26e6af3352058d2a131b3f729
Xiaofei <13263397018@163.com>
2019-11-12 16:40:59 +0800

    增加并发编程demo

--- concurrentDemo/GoroutineTest/ContextDemo.go
+++ concurrentDemo/GoroutineTest/ContextDemo.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"sync"
+)
+
+func main() {
+	wg := sync.WaitGroup{}
+	ctx, cancel := context.WithCancel(context.Background())
+
+	wg.Add(1)
+	go func(ctx context.Context) {
+		defer wg.Done()
+		for {
+			select {
+			case <-ctx.Done():
+				fmt.Println("Err:", ctx.Err())
+				return
+			default:
+			}
+		}
+	}(ctx)
+
+	cancel()
+	wg.Wait()
+}
--- concurrentDemo/GoroutineTest/TimerDemo.go
+++ concurrentDemo/GoroutineTest/TimerDemo.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	/*	timer1 := time.NewTimer(2*time.Second)
+		value := <-timer1.C
+		fmt.Println(value)
+		fmt.Println("Time 1 experied")*/
+
+	timer2 := time.NewTimer(2 * time.Second)
+	go func() {
+		<-timer2.C
+		fmt.Println("Time 2 experied")
+	}()
+	time.Sleep(1 * time.Second)
+	stop := timer2.Stop()
+	if stop {
+		fmt.Println("timer2 stoped")
+	}
+
+	ticker1 := time.NewTicker(time.Millisecond * 500)
+	go func() {
+		for t := range ticker1.C {
+			fmt.Println("Tick at ", t)
+		}
+	}()
+	time.Sleep(10 * time.Second)
+}
