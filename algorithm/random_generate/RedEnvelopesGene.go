990a46ae42ea9a58396be345e2e5aacee9b1679d
Xiaofei <13263397018@163.com>
2019-11-15 14:10:54 +0800

    增加新的分配算法和测试用例

--- algorithm/random_generate/RedEnvelopesGene.go
+++ algorithm/random_generate/RedEnvelopesGene.go
@@ -13,6 +13,38 @@ func Decimal(value float64) float64 {
 }
 
 /**
+returnSlice := make([]float64, n)
+	for k, v := range redPocketResult {
+		returnSlice[k] = Decimal(minMoney + v)
+	}
+	lastPacket := allMoney - sliceSum(returnSlice)
+	returnSlice[n-1] = Decimal(lastPacket + minMoney)
+	return returnSlice[:]
+*/
+func RedPacket2(n int, allMoney float64, minMoney float64) []float64 {
+	redPocketResult := make([]float64, n)
+	//可分配的钱数为 总钱数-n*每个钱包的最小数
+	canDisMoney := Decimal(allMoney - float64(n)*minMoney)
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < n-1; i++ {
+		coefficient := Decimal(rand.Float64()) //生成0.0-1.0随机整数
+		fmt.Printf("随机系数：%.2f\n", coefficient)
+		if coefficient == 0.0 || coefficient == 1.0 {
+			coefficient = 0.5
+		}
+		redPocketResult[i] = Decimal(coefficient * (canDisMoney / float64(n-i) * 2))
+		canDisMoney = Decimal(canDisMoney - redPocketResult[i])
+	}
+	returnSlice := make([]float64, n)
+	for k, v := range redPocketResult {
+		returnSlice[k] = Decimal(minMoney + v)
+	}
+	lastPacket := allMoney - sliceSum(returnSlice)
+	returnSlice[n-1] = Decimal(lastPacket + minMoney)
+	return returnSlice[:]
+}
+
+/**
   n : 红包个数
   allMoney ： 总钱数
    minMoney ：每个红包的最小钱数
@@ -52,7 +84,7 @@ func sliceSum(resultArray []float64) float64 {
 }
 
 func main() {
-	resultArray := RedPacket(10, float64(100), float64(0.01))
+	resultArray := RedPacket2(10, float64(100), float64(0.01))
 	fmt.Println(sliceSum(resultArray))
 	fmt.Println(resultArray)
 }
--- algorithm/random_generate/RedEnvelopesGene_test.go
+++ algorithm/random_generate/RedEnvelopesGene_test.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"testing"
+	"time"
 )
 
 /**
@@ -25,4 +27,86 @@ func TestRedPacket(t *testing.T) {
 			t.Errorf("Expect %f, but got %f!", expected, actual)
 		}
 	}
+}
+
+func TestRedPacket2(t *testing.T) {
+	startTime := time.Now().Second()
+	//随机发一万次红包
+	for i := 0; i < 10000; i++ {
+		resultArray := RedPacket2(10, float64(100), float64(0.01))
+		fmt.Println(resultArray)
+		for _, v := range resultArray {
+			if v < 0.01 {
+				t.Errorf("%f小于红包最小要求", v)
+			}
+		}
+		expected := 100.00
+		actual := sliceSum(resultArray)
+		if actual != expected {
+			t.Errorf("Expect %f, but got %f!", expected, actual)
+		}
+		time.Sleep(time.Millisecond)
+	}
+	fmt.Printf("程序执行时间：%d s\n", time.Now().Second()-startTime)
+}
+
+func RedPacket222(wg *sync.WaitGroup) {
+	defer wg.Done()
+	resultArray := RedPacket2(10, float64(100), float64(0.01))
+	fmt.Println(resultArray)
+	for _, v := range resultArray {
+		if v < 0.01 {
+			fmt.Printf("%f小于红包最小要求", v)
+		}
+	}
+	expected := 100.00
+	actual := sliceSum(resultArray)
+
+	if actual != expected {
+		fmt.Printf("Expect %f, but got %f!", expected, actual)
+	}
+	time.Sleep(time.Millisecond)
+}
+
+//并行测试
+//增加time.Sleep以后，性能差异显著
+func TestRedPacket3(t *testing.T) {
+	startTime := time.Now().Second()
+	wg := sync.WaitGroup{}
+	//随机发一万次红包
+	for i := 0; i < 10000; i++ {
+		wg.Add(1)
+		go RedPacket222(&wg)
+	}
+	wg.Wait()
+	fmt.Printf("程序执行时间：%d s\n", time.Now().Second()-startTime)
+}
+
+//匿名函数写法
+func TestRedPacket4(t *testing.T) {
+	startTime := time.Now().Second()
+	wg := sync.WaitGroup{}
+	//随机发一万次红包
+	for i := 0; i < 10000; i++ {
+		wg.Add(1)
+		go func(wgTemp *sync.WaitGroup) {
+			defer wg.Done()
+			resultArray := RedPacket2(10, float64(100), float64(0.01))
+			fmt.Println(resultArray)
+			for _, v := range resultArray {
+				if v < 0.01 {
+					fmt.Printf("%f小于红包最小要求", v)
+				}
+			}
+			expected := 100.00
+			actual := sliceSum(resultArray)
+
+			if actual != expected {
+				fmt.Printf("Expect %f, but got %f!", expected, actual)
+			}
+			time.Sleep(time.Millisecond)
+		}(&wg)
+	}
+	wg.Wait()
+	fmt.Printf("程序执行时间：%d s\n", time.Now().Second()-startTime)
 }
