91479c4c2762e6af114ae87c41be81ecfebe8c1c
Xiaofei <13263397018@163.com>
2019-12-05 10:38:17 +0800

    add json2struct demo

--- go.mod
+++ go.mod
@@ -3,10 +3,10 @@ module backend
 go 1.13
 
 require (
-	github.com/beego/bee v1.10.0 // indirect
 	github.com/gin-gonic/gin v1.4.0
+	github.com/golang/protobuf v1.3.2 // indirect
 	github.com/liyue201/gostl v0.0.0-20191107020652-707b2fe06409
-	google.golang.org/grpc v1.25.1 // indirect
+	github.com/mitchellh/mapstructure v1.1.2
 	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
 	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 )
---
+++ json/json2struct.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/mitchellh/mapstructure"
+)
+
+type Event struct {
+	Type     string              `json:"type"`
+	Database string              `json:"database"`
+	Table    string              `json:"table"`
+	Data     []map[string]string `json:"data"`
+}
+
+type Blog struct {
+	BlogId string `mapstructure:"blogId"`
+	Title  string `mapstructure:"title"`
+	Uid    int32  `mapstructure:"uid"`
+}
+
+func main() {
+	msg := []byte(`{
+       "type":"UPDATE",
+        "database":"blog",
+        "table":"blogs",
+         "data":[{
+          "blogId":"100001",
+          "title":"title",
+           "uid":"100"
+           }]
+
+     }`)
+	e := Event{}
+
+	if err := json.Unmarshal(msg, &e); err != nil {
+		panic(err)
+	}
+	fmt.Println(e)
+	if e.Table == "blogs" {
+		var blogs []Blog
+		//if err:= mapstructure.Decode(e.Data,&blogs); err!=nil{
+		if err := mapstructure.WeakDecode(e.Data, &blogs); err != nil {
+			panic(err)
+		}
+		fmt.Println(blogs)
+	}
+
+}
